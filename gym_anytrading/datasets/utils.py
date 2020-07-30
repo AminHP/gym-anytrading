64311dfa415e6d1c64203194cb6864c95dd971f7
AminHP <mdan.hagh@gmail.com>
2020-07-30 06:54:15 +0430

    Parse dates of the datasets

--- gym_anytrading/datasets/utils.py
+++ gym_anytrading/datasets/utils.py
@@ -5,4 +5,5 @@ import pandas as pd
 def load_dataset(name, index_name):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(base_dir, 'data', name + '.csv')
-    return pd.read_csv(path, index_col=index_name)
+    df = pd.read_csv(path, parse_dates=True, index_col=index_name)
+    return df
